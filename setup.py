268df3cb33d6c6b7d14cf74b71568bf18e006f3c
Ed Summers <ehs@pobox.com>
2014-10-04 08:07:49 -0400

    renamed to hathild

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Command
+
+class PyTest(Command):
+    """
+    A command to convince setuptools to run pytests.
+    """
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import pytest
+        pytest.main("test.py")
+
+setup(
+    name = 'hathild',
+    version = '0.0.1',
+    url = 'http://github.com/umd_mith/hathild',
+    author = 'Ed Summers',
+    author_email = 'ehs@pobox.com',
+    py_modules = ['hathild'],
+    description = 'Turn HathiTrust Data into JSON-LD',
+    cmdclass = {'test': PyTest},
+    install_requires = ['requests'],
+    tests_require=['pytest']
+)
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-import hathij
 import pytest
+import hathild
 
 def test_get_volume():
-    r = hathij.get('ucm.5305727634')
+    r = hathild.get('ucm.5305727634')
     assert r['title'] == ['Canon medicinae (latine), a Gerardo Cremonensi translatus ;']
 
